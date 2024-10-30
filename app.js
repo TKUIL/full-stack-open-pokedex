e7c9405f693e716f353dbc2f73bdfee7267b5509
TKUIL <153629020+TKUIL@users.noreply.github.com>
2024-10-30 18:33:42 +0200

    test broken deployment

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ app.listen(PORT, () => {
 
 app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ');
-  res.send('ok');
-});
+  if (true) throw('error...  ')
+  res.send('ok')
+})
